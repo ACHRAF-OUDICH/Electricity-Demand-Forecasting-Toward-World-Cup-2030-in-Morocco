6b935b96d266af1cb76388cf44b0990f226e4103
Achraf Oudich <achrafoudich9example.com>
2026-06-03 20:00:18 +0200

    Docs: Migrate home page to markdown index.md using myst_parser and fix encoding

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,22 +2,26 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
 
-project = "Prediction de la Demande d'Electricite - Maroc 2030"
+project = "Prévision de la Demande d'Électricité - Maroc 2030"
 copyright = '2026, Younes Chajara & Achraf Oudich'
 author = 'Younes Chajara & Achraf Oudich'
 release = 'latest'
 
+# L'extension 'myst_parser' permet à Sphinx de compiler le Markdown (.md)
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'myst_parser'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# Forcer la langue en fran�ais pour s'aligner avec les param�tres du build
 language = 'fr'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Forcer la lecture des fichiers source en UTF-8 pour éviter les '?' à la place des accents
+source_encoding = 'utf-8-sig'
