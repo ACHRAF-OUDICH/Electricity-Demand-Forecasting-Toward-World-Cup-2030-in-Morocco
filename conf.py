b9556e0a1107b34d717254db071ac4ebc3e60e62
Achraf Oudich <achrafoudich9example.com>
2026-06-03 20:12:02 +0200

    Docs: Automate root configuration to target README.md directly

---
+++ conf.py
@@ -0,0 +1,20 @@
+﻿import os
+import sys
+
+project = "Prévision de la Demande d'Électricité - Maroc 2030"
+copyright = '2026, Younes Chajara & Achraf Oudich'
+author = 'Younes Chajara & Achraf Oudich'
+release = 'latest'
+
+extensions = [
+    'myst_parser',
+]
+
+master_doc = 'README'
+source_suffix = {
+    '.md': 'markdown',
+}
+
+language = 'fr'
+html_theme = 'sphinx_rtd_theme'
+source_encoding = 'utf-8-sig'
